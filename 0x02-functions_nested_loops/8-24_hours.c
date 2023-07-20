348a3a0cbdebebbc3af1526d3f26f524209eaf25
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-07-20 21:26:02 +0200

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,10 @@ void jack_bauer(void)
 	while (h_1 < 3)
 	{
 		h_2 = 0;
-		while (h_2 < 4)
+		while (h_2 <= 9)
 		{
+			if ((h_1 == 2) && (h_2 ==4))
+				break;
 			m_1 = 0;
 			while (m_1 < 6)
 			{
