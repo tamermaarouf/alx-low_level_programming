278e6c630390466fd0109dd8e67b574e6620e1bd
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-07-20 21:07:19 +0200

    n

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+
+/**
+  * jack_bauer - Prints every minute of the day of Jack Bauer
+  *
+  * Return: ...
+  */
+
 void jack_bauer(void)
 {
 	int h_1, h_2, m_1, m_2;
 
 	h_1 = 0;
 
-	while(h_1 < 3)
+	while (h_1 < 3)
 	{
 		h_2 = 0;
 		while (h_2 < 4)
@@ -17,11 +24,11 @@ void jack_bauer(void)
 				m_2 = 0;
 				while (m_2 <= 9)
 				{
-					_putchar(h_1 + 48);
-					_putchar(h_2 + 48);
-					_putchar(58);
-					_putchar(m_1 + 48);
-					_putchar(m_2 + 48);
+					_putchar(48 + h_1);
+					_putchar(48 + h_2);
+					_putchar(':');
+					_putchar(48 + m_1);
+					_putchar(48 + m_2);
 					_putchar('\n');
 					++m_2;
 				}
