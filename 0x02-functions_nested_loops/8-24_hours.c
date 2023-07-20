da9dec4ff426f96494d4a62124468e53cc6039a5
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-07-20 20:55:28 +0200

    V

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,26 +2,26 @@
 
 void jack_bauer(void)
 {
-	char h_1, h_2, m_1, m_2;
+	int h_1, h_2, m_1, m_2;
 
-	h_1 = 48;
+	h_1 = 0;
 
-	while(h_1 < 51)
+	while(h_1 < 3)
 	{
-		h_2 = 48;
-		while (h_2 < 52)
+		h_2 = 0;
+		while (h_2 < 4)
 		{
-			m_1 = 48;
-			while (m_1 < 54)
+			m_1 = 0;
+			while (m_1 < 6)
 			{
-				m_2 = 48;
-				while (m_2 <= 57)
+				m_2 = 0;
+				while (m_2 <= 9)
 				{
-					_putchar(h_1);
-					_putchar(h_2);
+					_putchar(h_1 + 48);
+					_putchar(h_2 + 48);
 					_putchar(58);
-					_putchar(m_1);
-					_putchar(m_2);
+					_putchar(m_1 + 48);
+					_putchar(m_2 + 48);
 					_putchar('\n');
 					m_2++;
 				}
