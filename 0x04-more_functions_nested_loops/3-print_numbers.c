3dc64b71c41b3800051152528c7889d76d001b6b
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-07-22 21:57:01 +0200

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the number from 0 to 9.
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int index;
+
+	for (index = 48; index <= 57; ++index)
+		_putchar(index);
+	_putchar('\n');
+}
