6373f5fe53be1dc3a5391b717467db836b8a4b5a
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-07-28 13:11:24 +0200

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	long int n1, n2, sum, index;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%lu, ", n1);
+	printf("%lu, ", n2);
+	sum = n1 + n2;
+	index = sum;
+	while (index <= 98)
+	{
+		if (index == 50)
+			printf("%lu", sum);
+		else
+		{
+			printf("%lu, ", sum);
+			n1 = n2;
+			n2 = sum;
+			sum = n1 + n2;
+		}
+		++index;
+	}
+	printf("\n");
+
+	return (0);
+}
+
