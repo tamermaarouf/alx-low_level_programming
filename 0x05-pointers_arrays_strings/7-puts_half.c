18a47ee527468f07f8a5fe7d5b78981fb6677622
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-07-29 20:37:47 +0200

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int count, index;
+
+	count = _strlen(str);
+	if (count % 2 == 0)
+		index = count / 2;
+	else
+		index = (count - 1) / 2;
+	while (index <= count)
+	{
+		_putchar(*(str + index));
+		++index;
+	}
+}
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*s)
+	{
+		++length;
+		++s;
+	}
+	return (length);
+}
