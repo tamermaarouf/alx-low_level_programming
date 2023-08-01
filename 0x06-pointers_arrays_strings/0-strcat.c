a9c4f93587bf3d275253b1c53266720271017006
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-08-01 21:09:25 +0200

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 char *_strcat(char *dest, char *src)
 {
 	int index, count;
 
 	index = 0;
-	while (*(dest + index) != '\0')
+	while (*(dest + index))
 		++index;
 
 	count = 0;
-	while (*(src + count) != '\n')
+	while (*(src + count))
 	{
 		*(dest + index) = *(src + count);
 		if (*(src + count) == '\0')
