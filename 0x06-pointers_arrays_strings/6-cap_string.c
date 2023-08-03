f6a93646c0c90b4abb0a16b65c372eaf77a15bbc
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-08-03 20:02:10 +0200

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 /**
  *cap_string - capitalizes all words in a string
@@ -11,18 +13,17 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	bool flag;
 
 	while (*(s + i) != 0)
 	{
+		flag = separator_of_word(&s[i]);
 		if (*(s + i) == s[0])
 		{
 			if (s[i] >= 97 && s[i] <= 122)
 				*(s + i) -= 32;
 		}
-		if (s[i] == 32 || s[i] == 9 || s[i] == 10 ||
-		    s[i] == 44 || s[i] == 59 || s[i] == 46 || s[i] == 33 ||
-		    s[i] == 63 || s[i] == 34 || s[i] == 40 || s[i] == 41 ||
-		    s[i] == 123 || s[i] == 125)
+		if (flag)
 		{
 			i++;
 			if (s[i] >= 97 && s[i] <= 122)
@@ -35,20 +36,29 @@ char *cap_string(char *s)
 	return (s);
 }
 
-bool separator_of_word(char *c)
+/**
+ * separator_of_word - Separators of words: space, tabulation, new line
+ * @c: param for check
+ *
+ * Return: 0 or 1
+ */
+
+char separator_of_word(char *c)
 {
 	int index = 0;
+	bool flag;
 	char tabulation[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
 	while (*(tabulation + index) != '\0')
 	{
 		if (*(tabulation + index) == *c)
-			return (1);
+		{
+			flag = 1;
+			break;
+		}
 		else
-			return (0);
+			flag = 0;
 		++index;
 	}
+	return (flag);
 }
-
-
-
