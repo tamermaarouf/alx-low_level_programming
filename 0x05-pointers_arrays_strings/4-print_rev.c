70accd5dc76ce82839f7b50a809943b7beddb4b2
Tamer Ma3rouf <tamermaarouf@gmail.com>
2023-10-25 00:53:20 +0300

    "ffff"

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,64 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * print_rev - prints a string in reverse.
- * @s: param to reverse
- *
- * Return: void
- */
-=======
- * print_rev - reverse string.
- * @s: param of string.
- *
- * Return: void
- */
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
 
->>>>>>> 22bacc504f46e0a32c727ad04a045a24d71b6ac3
 void print_rev(char *s)
 {
-	int index;
+	int len;
 
-	index = _strlen(s) - 1;
-	while (index >= 0)
+	len = _strlen(s) - 1;
+	while (len >= 0)
 	{
-		_putchar(*(s + index));
-<<<<<<< HEAD
-		index--;
-=======
-		--index;
->>>>>>> 22bacc504f46e0a32c727ad04a045a24d71b6ac3
+		_putchar(s[len]);
+		--len;
 	}
 	_putchar('\n');
 }
 
 /**
-<<<<<<< HEAD
- * _strlen - calculate length if string 
- * @s: param to calculate
-=======
- * _strlen - calculate length of string
- * @s: param of string
->>>>>>> 22bacc504f46e0a32c727ad04a045a24d71b6ac3
- *
- * Return: length of string
- */
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
 
 int _strlen(char *s)
 {
-<<<<<<< HEAD
-	int length;
+	int count = 0;
 
-	length = 0;
-	while (*s++)
-		length++;
-	return (length);
-=======
-	int counter;
-
-	counter = 0;
-	while (*s++)
-		counter++;
-	return (counter);
->>>>>>> 22bacc504f46e0a32c727ad04a045a24d71b6ac3
+	while (*s)
+	{
+		++count;
+		s++;
+	}
+	return (count);
 }
