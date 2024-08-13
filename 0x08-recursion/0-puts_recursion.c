3b0d8900c86a7745f156f592693266c1bd981b52
tamer_maarouf <tamermaarouf@gmail.com>
2024-08-13 17:32:23 +0300

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
