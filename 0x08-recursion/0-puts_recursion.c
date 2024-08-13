47a5285b2c4271428bc38da73faacbb5f6e504c0
tamer_maarouf <tamermaarouf@gmail.com>
2024-08-13 17:38:05 +0300

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s)
+	if (*s == 0)
 	{
-		_putchar(*s);
-		s++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
