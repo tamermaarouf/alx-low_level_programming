e167bd454d2cf337b9479fcbd64e67aa3c6091d3
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-30 22:05:35 +0300

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: count of argument
+ * @argv: value of argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int mulu = 0;
+
+	mulu = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mulu);
+	return (0);
+}
