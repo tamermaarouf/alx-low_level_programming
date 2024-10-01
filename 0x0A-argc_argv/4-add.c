13681c8e1851952851a609c2b3acd52be128db2e
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-01 03:28:42 +0300

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - a program that adds positive numbers.
+ * @argc: count of argument
+ * @argv: value of argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int sum;
+
+	sum = 0;
+	if (argc < 2)
+	{
+		printf("%c\n", 48);
+		return (0);
+	}
+	while (argc-- >= 2)
+	{
+		if ((*argv[argc] < '0') || (*argv[argc] > '9'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[argc]);
+	}
+	printf("sum= :%i\n", sum);
+	return (0);
+}
