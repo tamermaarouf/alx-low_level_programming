6cd1584a0178667e698e4d5d3677ac6e38b0e339
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-04 12:04:02 +0300

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,12 +2,12 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	u_int8_t *ptr;
+	char *ptr;
 	unsigned int index = 0;
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
-	ptr = (u_int8_t *)malloc(nmemb * size);
+	ptr = (char *)malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 	while (index < nmemb)
