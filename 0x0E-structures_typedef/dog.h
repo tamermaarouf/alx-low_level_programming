eaacefdd8b8afb8e1028808bc112ccbccb6a2ab8
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-26 10:28:42 +0300

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
+#ifndef dog_H
+#define dog_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name dog
+ * @age: age
+ * @owner: owner
+ * description: Define a new type struct dog with the following elements
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
