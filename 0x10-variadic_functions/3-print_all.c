2a485b0b0d670295288a67c9fa7cc83b4b038ed3
tamer_maarouf <tamermaarouf@gmail.com>
2024-11-05 18:32:39 +0200

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,9 +6,17 @@ void print_digit(int);
 void print_str(char *);
 void print_float(double);
 
+/**
+ * get_op_func - funtion get function to print
+ * @s: list of type format
+ * @ap: pionter too list
+ * @fl: flag
+ * Return: void
+ */
+
 void get_op_func(char s, va_list ap, int *fl)
 {
-	switch(s)
+	switch (s)
 	{
 		case 'c':
 			print_char(va_arg(ap, int));
@@ -30,7 +38,11 @@ void get_op_func(char s, va_list ap, int *fl)
 			*fl = 0;
 	}
 }
-
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ * Return: no return.
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
@@ -43,17 +55,29 @@ void print_all(const char * const format, ...)
 	{
 		s = *(format + i);
 		get_op_func(s, args, &flag);
-		if ((flag) && (*(format + i + 1) != '\0')) { printf(", "); }
+		if ((flag) && (*(format + i + 1) != '\0')) 
+            printf(", ");
 		++i;
 	}
 	va_end(args);
 	printf("\n");
 }
+/**
+ * print_char - print character.
+ * @c: char to rint
+ * Return: void
+ */
 
 void print_char(int c)
 {
 	printf("%c", c);
 }
+/**
+ * print_str - print string
+ * @s: param to print string
+ * Return: void
+ */
+
 void print_str(char *s)
 {
 	if (!s)
@@ -63,10 +87,22 @@ void print_str(char *s)
 	}
 	printf("%s", s);
 }
+/**
+ * print_digit - print integer
+ * @a: param print
+ * Retrun: void
+ */
+
 void print_digit(int a)
 {
 	printf("%i", a);
 }
+/**
+ * print_float - print float number
+ * @d: param to print 
+ * Return: void
+ */
+
 void print_float(double d)
 {
     printf("%f", d);
