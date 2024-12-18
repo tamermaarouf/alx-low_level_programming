2f4ffd431da278574afcfb0acb6cd96b8b5a19b5
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-18 17:04:15 +0200

    [200~1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 1 << 10;
-	unsigned int index = 0;
-
-	while (index <= 10)
-	{
-		_putchar(n & mask ? '1' : '0');
-		n <<= 1;
-		index++;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
