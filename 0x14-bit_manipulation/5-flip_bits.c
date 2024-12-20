1d7c2f6ee41e4dffb84702db6051b635b71db45b
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-21 01:03:12 +0200

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * flip_bits - flip bits to convert one number to another number
+ * @n: first number
+ * @m: second number to convert to
+ * Return: number of bits that was needed to flip
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff;
+	unsigned int counter = 0;
+
+	diff = n ^= m;
+
+	while (diff)
+	{
+		printf("%lu\n", diff);
+		counter++;
+		diff &= (diff - 1);
+	}
+	return (counter);
+}
