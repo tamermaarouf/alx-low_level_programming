9fda5379b0e1d46a864374d99edb5244dc8d83d4
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2025-04-01 14:16:39 +0200

    Fix

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
     print_times_table(0);
+    /*
     _putchar('\n');
     print_times_table(3);
     _putchar('\n');
@@ -16,6 +17,7 @@ int main(void)
     print_times_table(98);
     _putchar('\n');
     print_times_table(12);  
+    */
     return (0);
 }
 
