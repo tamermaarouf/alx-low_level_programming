96c0657c19bd006458e2803a072676e3b0868e39
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2025-04-07 15:38:19 +0200

    K

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,7 +15,7 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int index_n1, index_n2, len_n1, len_n2, sum, a, b, reminder;
+	int index_n1, len_n1, len_n2, sum, a, b, reminder;
 
 	len_n1 = _strlen(n1) - 1;
 	len_n2 = _strlen(n2) - 1;
@@ -24,29 +24,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		index_n1 = len_n2;
 	if ((index_n1 + 1) >= size_r)
-		return 0;
+		return (0);
 	r[(index_n1 + 1)] = '\0';
 	index_n2 = 0, a = 0, b = 0, reminder = 0, sum = 0;
 	while (index_n1 >= 0)
 	{
-		printf("index = %d, size_r = %d\n", index_n1, size_r);
 		a = (n1[len_n1]);
 		b = (n2[len_n2]);
 		if ((len_n1 >= 0) && (len_n2 >= 0))
-		{
-			printf("n1 = %c, n2 = %c\n", a, b);
 			sum = (a - 48) + (b - 48) + reminder;
-		}
 		else if (len_n1 > 0 && len_n2 < 0)
-		{
-			printf("n1 = %c\n", a);
 			sum = (a - 48) + reminder;
-		}
 		else if (len_n1 < 0 && len_n2 > 0)
-		{
-			printf("n2 = %c\n", b);
 			sum = (b - 48) + reminder;
-		}
 		else
 			sum = reminder;
 		if (sum > 9)
@@ -60,14 +50,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum = (sum + 48);
 		}
 		r[index_n1] = sum;
-		printf("r = %c\n", (r[index_n1]));
 		len_n1--;
 		len_n2--;
-		index_n2--;
 		index_n1--;
 	}
-
-	return r;
+	return (r);
 }
 
 /**
