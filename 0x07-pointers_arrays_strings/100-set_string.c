1c3941235d9b0cf6a47d0eff632ed7f6d8b767ee
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2025-04-09 01:14:24 +0200

    8. Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * set_string - function that  sets the value of a pointer to a char.
+ * *@s: first value -char
+ * @to: second value -int
+ * Return: void
+ */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
